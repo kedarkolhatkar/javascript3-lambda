5e6ab08e187bf15c4eb9ce0c6b26f35c4f723b68
Kedar Kolhatkar <kkolhatkar@collegeboard.org>
2020-12-23 22:24:52 -0500

    javascriptt v3 api added

--- src/function/stack-event-processor.js
+++ src/function/stack-event-processor.js
@@ -1,3 +1,17 @@
+import { S3Client, ListBucketsCommand } from '@aws-sdk/client-s3';
+
+async function listObjects() {
+  try {
+    console.log('listObjects called');
+    const client = new S3Client({ region: 'us-east-1' });
+    const command = new ListBucketsCommand({});
+    const results = await client.send(command);
+    console.log(`results: ${JSON.stringify(results)}`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 async function waitFor() {
   console.log('waitFor called');
   return Promise.resolve('success');
@@ -7,6 +21,7 @@ const handler = async (event) => {
   console.log('Lambda function called');
   console.log(`event: ${JSON.stringify(event)}`);
   await waitFor();
+  await listObjects();
   console.log('executing after waitFor');
 };
 
